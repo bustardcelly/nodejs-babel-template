4bff431c7edf06f5db98242e8bc7add2c4ca0afb
Todd Anderson <bustardcelly@gmail.com>
2016-01-27 21:40:56 -0500

    adding in use of assertions

--- features-es2015/step_definitions/babel-test-steps.js
+++ features-es2015/step_definitions/babel-test-steps.js
@@ -1,13 +1,21 @@
 'use strict';
 
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonchai from 'sinon-chai';
+
 import {World} from '../support/world';
+
+const expect = chai.expect
+chai.use(sinonchai);
 
 export default function () {
 
   this.World = World;
   this.Given(/^I have a module I want imported$/, function(callback) {
     console.log('foo bar baz, accordingly' + this.punctuation);
-    callback.pending();
+    expect(this.punctuation).to.equal('!');
+    callback();
   });
 
 };
--- features/step_definitions/babel-test-steps.js
+++ features/step_definitions/babel-test-steps.js
@@ -9,11 +9,29 @@ exports.default = function () {
   this.World = _world.World;
   this.Given(/^I have a module I want imported$/, function (callback) {
     console.log('foo bar baz, accordingly' + this.punctuation);
-    callback.pending();
+    expect(this.punctuation).to.equal('!');
+    callback();
   });
 };
 
+var _chai = require('chai');
+
+var _chai2 = _interopRequireDefault(_chai);
+
+var _sinon = require('sinon');
+
+var _sinon2 = _interopRequireDefault(_sinon);
+
+var _sinonChai = require('sinon-chai');
+
+var _sinonChai2 = _interopRequireDefault(_sinonChai);
+
 var _world = require('../support/world');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var expect = _chai2.default.expect;
+_chai2.default.use(_sinonChai2.default);
 
 ;
 module.exports = exports['default'];
